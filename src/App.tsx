ce15b5361cf86b8121610db3ac1148cb8883e59d
Paulo Cesar Fordelone <pc@fordelone.com.br>
2022-07-18 17:49:07 -0300

    config styled-components, global style and defaultTheme

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,13 @@
+import { ThemeProvider } from "styled-components";
+import { GlobalStyle } from "./styles/global";
+import { defaultTheme } from "./styles/themes/default";
+
 export const App = () => {
-  return <h1>Ignite Timer</h1>;
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <h1>Ignite Timer</h1>
+
+      <GlobalStyle />
+    </ThemeProvider>
+  );
 };
