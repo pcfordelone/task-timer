29ddb29f20460ab673268c6e79aaa6f104eaa487
Paulo Cesar Fordelone <pc@fordelone.com.br>
2022-07-31 12:55:02 -0300

    layout, theme and lib update

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
+import { BrowserRouter } from 'react-router-dom'
 import { ThemeProvider } from 'styled-components'
+import { Router } from './Routes'
 import { GlobalStyle } from './styles/global'
 import { defaultTheme } from './styles/themes/default'
 
 export const App = () => {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <h1>Ignite Timer</h1>
-
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
   )
