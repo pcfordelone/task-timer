2a069d8b9c0fd81cf75f65af1b5f25860ba16353
Paulo Cesar Fordelone <pc@fordelone.com.br>
2022-07-31 12:55:17 -0300

    routes update

---
+++ src/Routes.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import { Route, Routes } from 'react-router-dom'
+import { DefaultLayout } from './layouts/DefaultLayout'
+import { History } from './pages/History'
+import { Home } from './pages/Home'
+
+export const Router: React.FC = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<DefaultLayout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/history" element={<History />} />
+      </Route>
+    </Routes>
+  )
+}
